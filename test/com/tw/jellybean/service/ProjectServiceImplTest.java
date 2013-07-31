fbb55b6c60754d6f90195ee3678f09cdaae9a9f6
indraneelr <indraneel.rr@gmail.com>
2013-07-31 16:59:57 +0530

    added tests to save ProjectList

--- test/com/tw/jellybean/service/ProjectServiceImplTest.java
+++ test/com/tw/jellybean/service/ProjectServiceImplTest.java
@@ -4,7 +4,12 @@ import com.tw.jellybean.dao.ProjectDao;
 import com.tw.jellybean.model.Project;
 import org.junit.Before;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class ProjectServiceImplTest {
@@ -22,11 +27,14 @@ public class ProjectServiceImplTest {
         ProjectService projectService =  new ProjectServiceImpl(mockProjectDao);
         Project project = new Project();
         projectService.save(project);
+        verify(mockProjectDao).save(project);
     }
 
     @org.junit.Test
     public void shouldSaveAllProjects() throws Exception {
-
-
+        ProjectService projectService =  new ProjectServiceImpl(mockProjectDao);
+        List<Project> projectList = new ArrayList<Project>();
+        projectService.save(projectList);
+        verify(mockProjectDao).save(projectList);
     }
 }
