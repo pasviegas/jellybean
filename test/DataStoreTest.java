e00353f1b0c7e6a88503be891e4dd1d2def12884
somisettynaresh <somisettynaresh@gmail.com>
2013-07-31 16:19:39 +0530

    DataStore Test

---
+++ test/DataStoreTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: somisetn
+ * Date: 31/07/13
+ * Time: 3:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataStoreTest {
+}
